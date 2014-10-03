0dcedd4d9afdc4ab79484f30115e8135fe042ec6
Lynn <lynn291991@yahoo.com>
2014-10-03 14:50:43 -0400

    Put the code in to OOP format

--- Program Challenge1/ReadCSV.php
+++ Program Challenge1/ReadCSV.php
@@ -1,52 +1,56 @@
 <?php
 
+ini_set('display_errors','on');
+error_reporting(E_ALL | E_STRICT);
+
 // Create a class to read CSV files and display data in tables formate 
 
 class rFile 
 {
 	// defining variables 
-	public $fname;
-	public $heading;
+	//public $fname;
+	//public $heading;
 	// open CSV file
-	public function readCSV($fname,$heading) {
+public static function readCSV($fname,$heading) {
+		$first_row = TRUE;
 		ini_set('auto_detect_line_endings',TRUE);
-		
-		// need a handler. Open the $fname file and use r for read mode 
 		if (($handle = fopen($fname, "r")) !== FALSE) {
-			while (($row = fgetcsv($handle, 1000, ",")) !== FALSE) {
-				if($heading == TRUE) {
-					$column_heading = $row;
-					$heading = FALSE;
-			} else {
-					$record = array_combine($column_heading, $row);
-					$records[] = $record;
-					}		
+    		while (($row = fgetcsv($handle, 1000, ",")) !== FALSE) {
+     		if($first_row == TRUE) {
+       		$column_heading = $row;
+       		$first_row = FALSE;
+     	} 
+     	else 
+     	{
+       $record = array_combine($column_heading, $row);
+       $records[] = $record;
+     }
 
       
-				}		
+    }
 
-					fclose($handle);
-					
-			}
-		
-		else {
-			
-				//could not open file 
-				echo "Failed to open the file" . $fname;
-		}
+    fclose($handle);
+	}
+		return $records;	
+}
+	public static function main($records){
 		// To link to the records
 		if (empty($_GET)){
-
+			
+			$i = -1;
+		//print_r($records);
 			foreach($records as $record){
 				$i++;
 				//$University_name= $i -1;
 			
-				echo '<a href = '.' "http://web.njit.edu/~lw234/is218/Program Challenge1/ReadCSV.php?record=' . $i . '"'. '>'.$record['INSTNM']. '</a>';
-	
+				echo '<a href = '.' "http://web.njit.edu/~lw234/is218/Program Challenge1/ReadCSV.php?record=' . $i . '"'. '>'.$record['Institution (entity) name']. '</a>';
+				//echo '<a href = '.' "http://web.njit.edu/~lw234/is218/Program Challenge1/ReadCSV.php?record=' . $University_name. '"'. '> University '.$i. '</a>';
+				//echo '<a href = '.' "http://web.njit.edu/~lw234/is218/Program Challenge1/ReadCSV.php?record=' .$i. '</a>';
 				echo '</p>';
 				}
 		}
-		
+		else{
+
 		//Getting records and echo the key and value for each record
 		$record = $records[$_GET['record']];
 		echo "<table border = 1 bordercolor= black cellspacing=0 cellpadding=5 style='font-size:14pt'>";
@@ -61,40 +65,48 @@ class rFile
 	
 			
 		}
+		
 		echo "</table>";
 	
- 			
-		/*
-		//echo "<table>\n";
-		//if($heading){
-				
-			//foreach($keys=(array_keys($records[0]))as $key){
-						
-						//echo "<th>$key</th>";
-						
-					//}
+	
+	}
+	}
+
+	public static function ReplaceHead($var){
 		
-				//display data
-			foreach($records as $record) {
-				
-			 	 //echo "<tr>";
-				foreach($record as $key => $value) {
-					
-					echo $key . ':'.$value ;
-				}						
-					//echo "<td>$value</td>";
-			//echo $key . ': ' . $value .  "</br> \n";
-	*/	//}
-		//echo '<hr>';
-		//echo "</tr>";
+	$Crecord=rFile::readCSV('varlist.csv',TRUE);
+	
+	foreach ($Crecord as $records){
+		
+		$replace[$records["varname"]]=  $records["varTitle"];
 	}
 	
-	//echo "</table>";
+		foreach($var as $krecords ){
+			
+			  $kr[] = array_combine($replace,$krecords );
+			  
+			 
+		}
+		return $kr;	
+	}
+	 	
+	}
+
+/*
+	public function csvrecord($records){
+  		foreach($records as $record) {
+    		foreach($record as $key => $value) {
+     		echo $key . ': ' . $value .  "</br> \n";
+        
+    		}
+    			echo '<hr>';
+    		}
+ }*/
 	
-	//}
-	
-}
-//Create objects and intantiate a class
-$obj1 = new rFile();
-$obj1->readCSV('hd.csv',TRUE);
+//calling the readCSV function;
+$var=rFile::readCSV('hd100.csv',TRUE);
+//displaying the data
+$var=rFile::ReplaceHead($var);
+rFile::main($var);
+
 ?>
